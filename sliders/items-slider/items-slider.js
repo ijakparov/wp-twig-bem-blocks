da6ec8de84cd0dc7ae771f6655f2f94a1ad141f6
Ildar Jakparov <ijakparov@gmail.com>
2017-01-10 10:55:29 +0500

    add timber blocks and custom slides number

--- sliders/items-slider/items-slider.js
+++ sliders/items-slider/items-slider.js
@@ -1,15 +1,22 @@
 var items_slider = function(){
 	return{
 		init:function(){
-			jQuery('.items-slider').slick({
-				infinite: true,
-				slidesToShow: 5,
-				slidesToScroll: 1,
-				slide: ".items-slider__wrapper",
-				arrows:true,
-				prevArrow:"<div class='slider-arrow slider-arrow--left'><i class='fa fa-angle-left' aria-hidden='true'></i></div>",
-				nextArrow:"<div class='slider-arrow slider-arrow--right'><i class='fa fa-angle-right' aria-hidden='true'></i></div>",
-				//variableWidth: true
+			
+			jQuery('.items-slider').each(function(){
+				var slides = 5;
+				if(jQuery(this).data('slides')){
+					slides = jQuery(this).data('slides');
+				}
+				jQuery(this).slick({
+					infinite: true,
+					slidesToShow: slides,
+					slidesToScroll: 1,
+					slide: ".items-slider__wrapper",
+					arrows:true,
+					prevArrow:"<div class='slider-arrow slider-arrow--left'><i class='fa fa-angle-left' aria-hidden='true'></i></div>",
+					nextArrow:"<div class='slider-arrow slider-arrow--right'><i class='fa fa-angle-right' aria-hidden='true'></i></div>",
+
+				});
 			});
 
 			jQuery('.items-slider').on('setPosition', function () {
